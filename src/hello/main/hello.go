e38fbbddcfcae221ac8580f356fb210d27c12ae9
GanZhiXiong <1551935335@qq.com>
2021-03-08 12:24:32 +0800

    Add goroutine test

--- src/hello/main/hello.go
+++ src/hello/main/hello.go
@@ -5,21 +5,39 @@ package main
 // IDE会自动导入包，所以可以直接使用fmt，而不需要写下面代码
 import (
 	"fmt"
-	"os"
+	"time"
 )
 
 // 程序入口，功能实现
-func main() {
-	//x := 200
-	//var any interface{} = x
-	//fmt.Println(any)
-	//return
+//func main() {
+//	//x := 200
+//	//var any interface{} = x
+//	//fmt.Println(any)
+//	//return
+//
+//	if len(os.Args) > 1 {
+//		fmt.Println("Args[1]: ", os.Args[1])
+//	}
+//	fmt.Println("Start")
+//	fmt.Println("Hello, World!")
+//	fmt.Println("End")
+//	os.Exit(1)
+//}
 
-	if len(os.Args) > 1 {
-		fmt.Println("Args[1]: ", os.Args[1])
+func running() {
+	var times int
+	for {
+		times++
+		fmt.Println("tick", times)
+
+		time.Sleep(time.Second)
 	}
-	fmt.Println("Start")
-	fmt.Println("Hello, World!")
-	fmt.Println("End")
-	os.Exit(1)
+}
+
+func main() {
+	go running()
+
+	// 接受命令行输入，不作任何事情
+	var input string
+	fmt.Scanln(&input)
 }
