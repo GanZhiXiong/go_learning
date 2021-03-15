45a65a8d8d924627778b3922072b3bb921af2bbd
GanZhiXiong <1551935335@qq.com>
2021-03-15 16:38:52 +0800

    添加WaitGroup测试用例

--- src/test/goroutine_channel/24_wait_group_test.go
+++ src/test/goroutine_channel/24_wait_group_test.go
@@ -1,6 +1,7 @@
 package goroutine_channel
 
 import (
+	"net/http"
 	"sync"
 	"testing"
 )
@@ -20,4 +21,31 @@ func TestWaitGroup(t *testing.T) {
 	t.Log("等待其他协程结束")
 	wg.Wait()
 	t.Log("其他协程已结束")
+}
+
+func TestWaitGroup1(t *testing.T) {
+	t.Log("start")
+
+	var wg sync.WaitGroup
+
+	var urls = []string{
+		"https://www.github.com/",
+		"https://www.baidu.com/",
+		"https://www.google.com/",
+	}
+
+	for _, url := range urls {
+		wg.Add(1)
+
+		go func(url string) {
+			defer wg.Done()
+
+			_, err := http.Get(url)
+			t.Log(url, err)
+		}(url)
+	}
+
+	wg.Wait()
+
+	t.Log("over")
 }
